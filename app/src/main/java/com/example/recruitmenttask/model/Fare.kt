ebb410f604f227109c6255779330560ad41fdb1f
Piotr Michałkiewicz <piotrek.michalkiewicz@gmail.com>
2021-03-31 20:03:34 +0200

    Removed hasBogof serialized name

--- app/src/main/java/com/example/recruitmenttask/model/Fare.kt
+++ app/src/main/java/com/example/recruitmenttask/model/Fare.kt
@@ -11,6 +11,5 @@ data class Fare(
     val discountInPercent: Int,
     val hasPromoDiscount: Boolean,
     val discountAmount: Double,
-    @SerializedName("hasBogof")
     val hasBogOf: Boolean
 )
